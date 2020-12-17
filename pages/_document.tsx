4cde4b73c04e43817082dd12560d92d29647d9f4
Giuseppe <ciotola@extendi.it>
2020-12-17 10:01:27 +0100

    add custom app document to import global fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  Html,
+  Main,
+  NextScript,
+  Head,
+  DocumentContext,
+} from "next/document"
+
+class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default AppDocument
