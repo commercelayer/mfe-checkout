95b81db2a416c2c4a22818ba81d64678e94e787b
Matteo Alessani <alessani@gmail.com>
2020-12-29 16:54:32 +0100

    rename typo in file name

--- components/data/AppProvider/index.tsx
+++ components/data/AppProvider/index.tsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from "react"
 
-import { fetchOrderById } from "./fetchOrderByOrder"
+import { fetchOrderById } from "./fetchOrderById"
 
 interface AppProviderData {
   hasBillingAddress: boolean
