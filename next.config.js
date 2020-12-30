73cca91eed426d067eae1395132af0f2db182324
Giacomo Sardo <63127363+giacomosardo@users.noreply.github.com>
2020-12-30 11:26:11 +0100

    added module to next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+const { nextI18NextRewrites } = require('next-i18next/rewrites')
+
+const localeSubpaths = {
+  en: 'en',
+  it: 'it',
+}
+
+module.exports = {
+  rewrites: async () => nextI18NextRewrites(localeSubpaths),
+  publicRuntimeConfig: {
+    localeSubpaths,
+  },
+}
+
 module.exports = {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
