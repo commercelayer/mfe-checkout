3904ed46c8af14b3691c22e43cb2dba63e40dac9
Matteo Alessani <alessani@gmail.com>
2020-12-30 17:57:35 +0100

    refactor default and other languages

--- components/data/i18n/index.ts
+++ components/data/i18n/index.ts
@@ -1,19 +1,22 @@
 import NextI18Next from "next-i18next"
 
+const defaultLanguage = "en"
+const otherLanguages = ["it"]
+const allLanguages = [defaultLanguage, ...otherLanguages]
+
 const NextI18NextIntance = new NextI18Next({
-  defaultLanguage: "en",
-  otherLanguages: ["it"],
+  defaultLanguage: defaultLanguage,
+  otherLanguages: otherLanguages,
   defaultNS: "common",
-  fallbackLng: "en",
+  fallbackLng: defaultLanguage,
   localePath: "public/static/locales",
 })
 
 export const { appWithTranslation, useTranslation } = NextI18NextIntance
 
 export function changeLanguage(languageCode: string) {
-  const languagesCode = ["it", "en"]
-  if (languagesCode.includes(languageCode)) return languageCode
-  return "en"
+  if (allLanguages.includes(languageCode)) return languageCode
+  return defaultLanguage
 }
 
 export default NextI18NextIntance
