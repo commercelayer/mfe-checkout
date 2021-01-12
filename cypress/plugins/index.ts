6522520a1c8a94ef6e4ddab04bfb34dc78d3e4d6
Giacomo Sardo <63127363+giacomosardo@users.noreply.github.com>
2021-01-12 18:49:18 +0100

    fixed from js to ts

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,21 @@
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+import axios from "axios"
+
+export default (on, config) => {
+  on("task", {
+    async setAccessTokenTest() {
+      const {
+        data: { access_token },
+      } = await axios.post(
+        "https://checkout-test.commercelayer.io/oauth/token?grant_type=client_credentials&client_id=HAA9HFHyqF-AUroyzK5jrXMX-zPrmEWK1kq9pj56jww&scope=market%3A3816"
+      )
+      config.env.sharedSecret = access_token
+      return config
+    },
+  })
+
+  return config
+}
