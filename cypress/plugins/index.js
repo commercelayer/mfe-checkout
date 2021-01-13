728697e986da9f84d5c54ef42e670868d553c354
Giacomo Sardo <63127363+giacomosardo@users.noreply.github.com>
2021-01-13 10:52:43 +0100

    added get automatically access token

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,18 +4,14 @@
 
 import axios from "axios"
 
-export default (on, config) => {
-  on("task", {
-    async setAccessTokenTest() {
-      const {
-        data: { access_token },
-      } = await axios.post(
-        "https://checkout-test.commercelayer.io/oauth/token?grant_type=client_credentials&client_id=HAA9HFHyqF-AUroyzK5jrXMX-zPrmEWK1kq9pj56jww&scope=market%3A3816"
-      )
-      config.env.sharedSecret = access_token
-      return config
-    },
-  })
-
+export default async (on, config) => {
+  if (process.env.NODE_ENV === 'test' && config.env.record) {
+    const {
+      data: { access_token },
+    } = await axios.post(
+      "https://checkout-test.commercelayer.io/oauth/token?grant_type=client_credentials&client_id=HAA9HFHyqF-AUroyzK5jrXMX-zPrmEWK1kq9pj56jww&scope=market%3A3816"
+    )
+    config.env.accessToken = access_token
+  }
   return config
 }
