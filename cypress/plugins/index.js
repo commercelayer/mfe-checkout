c51adbcb6ae75955e4490ff1e5e233b7865f126b
Giacomo Sardo <63127363+giacomosardo@users.noreply.github.com>
2021-01-13 11:04:58 +0100

    fixed path url

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,7 +9,7 @@ export default async (on, config) => {
     const {
       data: { access_token },
     } = await axios.post(
-      "https://checkout-test.commercelayer.io/oauth/token?grant_type=client_credentials&client_id=HAA9HFHyqF-AUroyzK5jrXMX-zPrmEWK1kq9pj56jww&scope=market%3A3816"
+      config.env.apiEndpoint + "/oauth/token?grant_type=client_credentials&client_id=" + config.env.clientId + "&scope=market%3A3816"
     )
     config.env.accessToken = access_token
   }
