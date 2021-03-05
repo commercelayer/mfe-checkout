7093f5760ba4d0885782c22c11762692e427d0b2
Giacomo Sardo <63127363+giacomosardo@users.noreply.github.com>
2021-03-05 13:36:58 +0100

    remove handleSubmit

--- components/composite/StepPayment/index.tsx
+++ components/composite/StepPayment/index.tsx
@@ -59,7 +59,6 @@ export const StepPayment: React.FC<Props> = ({
             config={{
               stripePayment: {
                 publishableKey: "pk_test_UArgJuzBMSppFkvAkATXTNT5",
-                handleSubmit: refetchOrder,
                 submitLabel: t("stepPayment.setPaymentMethod"),
                 submitClassName:
                   "mt-5 inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-primary border border-transparent shadow-sm rounded-md hover:opacity-80 disabled:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50",
