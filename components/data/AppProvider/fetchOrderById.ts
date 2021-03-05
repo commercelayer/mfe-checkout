a6ab316d71d5c0d44b1d470707665a8f1cd2e872
Giacomo Sardo <63127363+giacomosardo@users.noreply.github.com>
2021-03-05 14:07:59 +0100

    change endpoint env

--- components/data/AppProvider/fetchOrderById.ts
+++ components/data/AppProvider/fetchOrderById.ts
@@ -153,7 +153,7 @@ export const fetchOrderById = async ({
 }: FetchOrderByIdProps): Promise<FetchOrderByIdResponse> => {
   CLayer.init({
     accessToken: accessToken,
-    endpoint: `${process.env.NEXT_PUBLIC_API_DOMAIN}`,
+    endpoint: `${process.env.NEXT_CLAYER_DOMAIN}`,
   })
 
   try {
