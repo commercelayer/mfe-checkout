36657c3f7ef45a6ba6242e156890c118384bd8c3
Matteo Alessani <alessani@gmail.com>
2021-04-30 12:53:28 +0200

    refactor i18n import to _app

---
+++ components/hooks/useOrderOrInvalid.ts
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router"
+import useSWR from "swr"
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
+
+interface UseOrderOrInvalid {
+  data: CheckoutSettings | undefined
+  isLoading: boolean
+}
+
+export const useOrderOrInvalid = (): UseOrderOrInvalid => {
+  const router = useRouter()
+
+  const { data, error } = useSWR(
+    router.isReady ? `/api/settings${router.asPath}` : null,
+    fetcher
+  )
+
+  if (!data && !error) {
+    return { isLoading: true, data: undefined }
+  }
+
+  if (error) {
+    router.push("/invalid")
+  }
+
+  if (data && !data.validCheckout) {
+    router.push("/invalid")
+  }
+
+  return {
+    data,
+    isLoading: false,
+  }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import "../styles/globals.css"
-
-import i18n from "i18next"
 import { appWithTranslation } from "next-i18next"
 import type { AppProps } from "next/app"
-import translationEN from "public/static/locales/en/common.json"
-import translationIT from "public/static/locales/it/common.json"
-import { initReactI18next } from "react-i18next"
+import "components/data/i18n"
 
 if (
   process.env.NEXT_PUBLIC_API_MOCKING === "enabled" &&
@@ -16,23 +12,6 @@ if (
 
 function CheckoutApp(props: AppProps) {
   const { Component, pageProps } = props
-
-  const languages = ["en", "it"]
-
-  const resources = {
-    en: {
-      translation: translationEN,
-    },
-    it: {
-      translation: translationIT,
-    },
-  }
-
-  i18n.use(initReactI18next).init({
-    resources,
-    lng: languages[0],
-    fallbackLng: languages,
-  })
 
   return <Component {...pageProps} />
 }
