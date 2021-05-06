60968d1fa5364b3fee29b9c8403b69eb6427314a
Matteo Alessani <alessani@gmail.com>
2021-05-06 18:33:26 +0200

    change spinner to gray

--- components/ui/SpinnerLoader/index.tsx
+++ components/ui/SpinnerLoader/index.tsx
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components"
 export const SpinnerLoader: React.FC = () => {
   return (
     <Wrapper>
-      <Spinner sizepx={100} color="salmon">
+      <Spinner sizepx={100} color="gray">
         <span className="spinner-inner-1"></span>
         <span className="spinner-inner-2"></span>
         <span className="spinner-inner-3"></span>
