aef0714e28f3a7b2d49794bbfafaa70a9181ef9e
Berardo Matè <mate@extendi.it>
2021-05-11 10:25:15 +0200

    Button component

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components"
 import tw from "twin.macro"
 
 export const ButtonCss = css`
-  ${tw`inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-primary border border-transparent shadow-sm rounded-md hover:opacity-80 disabled:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50`}
+  ${tw`inline-flex items-center justify-center w-full p-3 text-xs font-extrabold text-white bg-primary border border-transparent rounded-md hover:opacity-80 disabled:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50 md:w-48`}
 `
 export const ButtonWrapper = styled.div`
   ${tw`flex justify-end`}
