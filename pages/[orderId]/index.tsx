649b8ced543d860724a9ff501b561a940ba4c826
Giacomo Sardo <63127363+giacomosardo@users.noreply.github.com>
2021-05-19 11:31:44 +0200

    add ErrorBoundary

--- pages/[orderId]/index.tsx
+++ pages/[orderId]/index.tsx
@@ -1,9 +1,9 @@
 import "twin.macro"
 import { CommerceLayer, OrderContainer } from "@commercelayer/react-components"
+import { Provider, ErrorBoundary } from "@rollbar/react"
 import { NextPage } from "next"
 import Head from "next/head"
 import { useTranslation } from "react-i18next"
-import Rollbar from "rollbar"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
 
 import { Checkout } from "components/composite/Checkout"
@@ -24,15 +24,14 @@ const GlobalCssStyle = createGlobalStyle<GlobalStyleProps>`
 `
 
 const Home: NextPage = () => {
-  const rollbar = new Rollbar({
+  const rollbarConfig = {
     accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
     captureUncaught: true,
     captureUnhandledRejections: true,
     payload: {
       environment: "production",
     },
-  })
-  rollbar.info("init rollbar")
+  }
 
   const { t } = useTranslation()
 
@@ -47,41 +46,45 @@ const Home: NextPage = () => {
         <title>{t("general.title")}</title>
         <link rel="icon" href={settings.favicon} />
       </Head>
-      <CommerceLayer
-        accessToken={settings.accessToken}
-        endpoint={settings.endpoint}
-      >
-        <GlobalCssStyle
-          primaryColor={settings.primaryColor}
-          contrastColor={settings.contrastColor}
-        />
-        <OrderContainer orderId={settings.orderId}>
-          <ThemeProvider
-            theme={{
-              colors: {
-                primary: settings.primaryColor,
-                contrast: settings.contrastColor,
-              },
-            }}
+      <Provider config={rollbarConfig}>
+        <ErrorBoundary>
+          <CommerceLayer
+            accessToken={settings.accessToken}
+            endpoint={settings.endpoint}
           >
-            <AppProvider
-              orderId={settings.orderId}
-              accessToken={settings.accessToken}
-              endpoint={settings.endpoint}
-            >
-              <GTMProvider gtmId={settings.gtmId}>
-                <Checkout
-                  logoUrl={settings.logoUrl}
-                  orderNumber={settings.orderNumber}
-                  companyName={settings.companyName}
-                  supportEmail={settings.supportEmail}
-                  supportPhone={settings.supportPhone}
-                />
-              </GTMProvider>
-            </AppProvider>
-          </ThemeProvider>
-        </OrderContainer>
-      </CommerceLayer>
+            <GlobalCssStyle
+              primaryColor={settings.primaryColor}
+              contrastColor={settings.contrastColor}
+            />
+            <OrderContainer orderId={settings.orderId}>
+              <ThemeProvider
+                theme={{
+                  colors: {
+                    primary: settings.primaryColor,
+                    contrast: settings.contrastColor,
+                  },
+                }}
+              >
+                <AppProvider
+                  orderId={settings.orderId}
+                  accessToken={settings.accessToken}
+                  endpoint={settings.endpoint}
+                >
+                  <GTMProvider gtmId={settings.gtmId}>
+                    <Checkout
+                      logoUrl={settings.logoUrl}
+                      orderNumber={settings.orderNumber}
+                      companyName={settings.companyName}
+                      supportEmail={settings.supportEmail}
+                      supportPhone={settings.supportPhone}
+                    />
+                  </GTMProvider>
+                </AppProvider>
+              </ThemeProvider>
+            </OrderContainer>
+          </CommerceLayer>
+        </ErrorBoundary>
+      </Provider>
     </div>
   )
 }
