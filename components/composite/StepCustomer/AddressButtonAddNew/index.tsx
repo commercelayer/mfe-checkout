ca2bf10c0d06409d14a433e64572b3f66e6722dc
Berardo Matè <mate@extendi.it>
2021-05-21 18:24:56 +0200

    Close new address form

--- components/composite/StepCustomer/AddressButtonAddNew/index.tsx
+++ components/composite/StepCustomer/AddressButtonAddNew/index.tsx
@@ -8,17 +8,17 @@ interface Props {
 export const AddressButtonAddNew: React.FC<Props> = ({ children, onClick }) => {
   return (
     <Wrapper>
-      <Button type="button" onClick={onClick} data-cy="add-new-billing-address">
+      <Button onClick={onClick} data-cy="add-new-billing-address">
         {children}
       </Button>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.h4`
+const Wrapper = styled.div`
   ${tw`flex  my-2`}
 `
 
-const Button = styled.button`
-  ${tw`inline-flex items-center px-4 py-2 text-sm font-medium text-primary border border-transparent rounded-md focus:outline-none focus:ring-2 hover:ring-2 ring-offset-2 ring-primary`}
+const Button = styled.p`
+  ${tw`inline-flex text-sm font-bold text-primary border-b leading-none border-black border-opacity-10 md: transition ease-in duration-200 hover:border-opacity-50 hover:text-primary-dark`}
 `
