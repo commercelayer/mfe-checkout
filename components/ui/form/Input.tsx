1239ad91dcf608f2456c7177fb7be63f13c24f74
Berardo Matè <mate@extendi.it>
2021-05-31 19:17:49 +0200

    Remove autofill background on input

--- components/ui/form/Input.tsx
+++ components/ui/form/Input.tsx
@@ -3,4 +3,12 @@ import tw from "twin.macro"
 
 export const InputCss = css`
   ${tw`block w-full border-gray-400 border rounded-md p-3 transition duration-500 ease-in-out focus:border-primary focus:ring focus:ring-offset-0 focus:ring-primary-light focus:ring-opacity-50  sm:text-sm`}
+
+  &:-webkit-autofill {
+    ${tw`shadow-inner bg-gray-900`}
+
+    &:focus {
+      ${tw`shadow-inner`}
+    }
+  }
 `
