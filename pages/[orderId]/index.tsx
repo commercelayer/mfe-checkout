76e6434dcf39821a3ef3e31c9daade1fc1a8c3ba
Matteo Alessani <alessani@gmail.com>
2021-07-19 19:03:44 +0200

    test skeleton with dynamic import

--- pages/[orderId]/index.tsx
+++ pages/[orderId]/index.tsx
@@ -1,18 +1,32 @@
 import { NextPage } from "next"
+import dynamic from "next/dynamic"
 
-import Checkout from "components/composite/Checkout"
-import CheckoutContainer from "components/composite/CheckoutContainer"
 import CheckoutSkeleton from "components/composite/CheckoutSkeleton"
 import { useSettingsOrInvalid } from "components/hooks/useSettingsOrInvalid"
 
+const DynamicCheckoutContainer = dynamic(
+  () => import("components/composite/CheckoutContainer"),
+  {
+    loading: function LoadingSkeleton() {
+      return <CheckoutSkeleton />
+    },
+  }
+)
+const DynamicCheckout = dynamic(() => import("components/composite/Checkout"), {
+  loading: function LoadingSkeleton() {
+    return <CheckoutSkeleton />
+  },
+})
+
+CheckoutSkeleton.displayName = "Skeleton Loader"
 const Home: NextPage = () => {
   const { settings, isLoading } = useSettingsOrInvalid()
 
   if (isLoading || !settings) return <CheckoutSkeleton />
 
   return (
-    <CheckoutContainer settings={settings}>
-      <Checkout
+    <DynamicCheckoutContainer settings={settings}>
+      <DynamicCheckout
         logoUrl={settings.logoUrl}
         orderNumber={settings.orderNumber}
         companyName={settings.companyName}
@@ -21,7 +35,7 @@ const Home: NextPage = () => {
         termsUrl={settings.termsUrl}
         privacyUrl={settings.privacyUrl}
       />
-    </CheckoutContainer>
+    </DynamicCheckoutContainer>
   )
 }
 
