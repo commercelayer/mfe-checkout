defada76d9af6c9e830dd69444bd71c3fee5d770
Berardo Matè <mate@extendi.it>
2021-11-18 18:29:55 +0100

    feat: link button mobile

--- components/composite/StepCustomer/AddressFormBottom/styled.tsx
+++ components/composite/StepCustomer/AddressFormBottom/styled.tsx
@@ -2,5 +2,5 @@ import styled from "styled-components"
 import tw from "twin.macro"
 
 export const Wrapper = styled.div`
-  ${tw`flex justify-between`}
+  ${tw`flex flex-col-reverse justify-between md:flex-row`}
 `
