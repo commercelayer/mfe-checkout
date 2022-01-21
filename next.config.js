fd58395f0ff08e20b9a92a48c3a5376bfd1dee59
Matteo Alessani <matteo@commercelayer.io>
2022-01-21 15:46:02 +0100

    build: add HSTS and DNS prefetch

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+]
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ]
+  },
   eslint: {},
   swcMinify: true,
   webpack: (config) => {
