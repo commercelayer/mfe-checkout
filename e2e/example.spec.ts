f5c02baaf73b8b93f78411b5d61719181d48bc5b
Matteo Alessani <matteo@commercelayer.io>
2022-01-31 15:25:55 +0100

    chore: remove test file and add debug command

--- e2e/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test"
-
-test("should navigate to the 404 page", async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("http://localhost:3000/")
-  // The new page should contain an h1 with "About Page"
-  await expect(page.locator("p")).toContainText("404")
-})
