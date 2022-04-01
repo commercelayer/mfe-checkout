add38664bc7ad901ef5dc0bffd5e59273696cb73
Matteo Alessani <matteo@commercelayer.io>
2022-04-01 14:18:17 +0200

    test: add braintree customer tests

--- specs/e2e/payments.spec.ts
+++ specs/e2e/payments.spec.ts
@@ -449,6 +449,60 @@ test.describe("guest with wire transfer", () => {
   })
 })
 
+test.describe("guest with Braintree", () => {
+  const customerEmail = faker.internet.email().toLocaleLowerCase()
+
+  test.use({
+    defaultParams: {
+      order: "with-items",
+      orderAttributes: {
+        customer_email: customerEmail,
+      },
+      lineItemsAttributes: [
+        { sku_code: "CANVASAU000000FFFFFF1824", quantity: 1 },
+      ],
+      addresses: {
+        billingAddress: euAddress,
+        sameShippingAddress: true,
+      },
+    },
+  })
+
+  test("Checkout order", async ({ checkoutPage }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.checkStep("Shipping", "open")
+
+    await checkoutPage.selectShippingMethod({ text: "Standard Shipping" })
+
+    await checkoutPage.save("Shipping")
+
+    await checkoutPage.selectPayment("braintree")
+
+    const element = await checkoutPage.page.locator(
+      "[data-test-id=payment-save-wallet]"
+    )
+    expect(element).not.toBeVisible()
+
+    await checkoutPage.setPayment("braintree")
+
+    await checkoutPage.save("Payment", undefined, true)
+
+    const cardinalFrame = checkoutPage.page.frameLocator(
+      "text=<head></head> <body> <div></div> </body>"
+    )
+    await cardinalFrame
+      .locator('[placeholder="\\ Enter\\ Code\\ Here"]')
+      .fill("1234")
+
+    await cardinalFrame.locator("text=SUBMIT").click()
+
+    await checkoutPage.page
+      .locator(`text=Thank you for your order!`)
+      .waitFor({ state: "visible", timeout: 100000 })
+  })
+})
+
 test.describe("customer with Braintree", () => {
   const customerEmail = faker.internet.email().toLocaleLowerCase()
   const customerPassword = faker.internet.password()
@@ -493,6 +547,73 @@ test.describe("customer with Braintree", () => {
 
     await checkoutPage.setPayment("braintree")
     await checkoutPage.checkButton({ type: "Payment", status: "enabled" })
+
+    await checkoutPage.save("Payment", undefined, true)
+
+    const cardinalFrame = checkoutPage.page.frameLocator(
+      "text=<head></head> <body> <div></div> </body>"
+    )
+    await cardinalFrame
+      .locator('[placeholder="\\ Enter\\ Code\\ Here"]')
+      .fill("1234")
+
+    await cardinalFrame.locator("text=SUBMIT").click()
+
+    await checkoutPage.page
+      .locator(`text=Thank you for your order!`)
+      .waitFor({ state: "visible", timeout: 100000 })
+  })
+
+  test("save card and checkout", async ({ checkoutPage }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.checkStep("Shipping", "open")
+
+    await checkoutPage.selectShippingMethod({ text: "Standard Shipping" })
+
+    await checkoutPage.save("Shipping")
+
+    await checkoutPage.selectPayment("braintree")
+
+    const element = await checkoutPage.page.locator(
+      "[data-test-id=payment-save-wallet]"
+    )
+    expect(element).toBeVisible()
+    expect(element).not.toBeChecked()
+    await element.check()
+
+    await checkoutPage.setPayment("braintree")
+
+    await checkoutPage.save("Payment", undefined, true)
+
+    const cardinalFrame = checkoutPage.page.frameLocator(
+      "text=<head></head> <body> <div></div> </body>"
+    )
+    await cardinalFrame
+      .locator('[placeholder="\\ Enter\\ Code\\ Here"]')
+      .fill("1234")
+
+    await cardinalFrame.locator("text=SUBMIT").click()
+
+    await checkoutPage.page
+      .locator(`text=Thank you for your order!`)
+      .waitFor({ state: "visible", timeout: 100000 })
+  })
+
+  test("with customer wallet", async ({ checkoutPage }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.checkStep("Shipping", "open")
+
+    await checkoutPage.selectShippingMethod({ text: "Standard Shipping" })
+
+    await checkoutPage.save("Shipping")
+
+    await checkoutPage.selectPayment("braintree")
+
+    await checkoutPage.page.click("[data-test-id=customer-card]", {
+      force: true,
+    })
 
     await checkoutPage.save("Payment", undefined, true)
 
