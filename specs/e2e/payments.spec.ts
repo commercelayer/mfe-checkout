9c698626fb3ecd175f718e2e588ebe2005d893d4
Matteo Alessani <matteo@commercelayer.io>
2022-04-05 18:45:20 +0200

    chore: update deps

--- specs/e2e/payments.spec.ts
+++ specs/e2e/payments.spec.ts
@@ -541,8 +541,8 @@ test.describe("customer with Braintree", () => {
     const element = await checkoutPage.page.locator(
       "[data-test-id=payment-save-wallet]"
     )
-    expect(element).toBeVisible()
-    expect(element).not.toBeChecked()
+    await expect(element).toBeVisible()
+    await expect(element).not.toBeChecked()
     await checkoutPage.checkButton({ type: "Payment", status: "disabled" })
 
     await checkoutPage.setPayment("braintree")
@@ -578,8 +578,8 @@ test.describe("customer with Braintree", () => {
     const element = await checkoutPage.page.locator(
       "[data-test-id=payment-save-wallet]"
     )
-    expect(element).toBeVisible()
-    expect(element).not.toBeChecked()
+    await expect(element).toBeVisible()
+    await expect(element).not.toBeChecked()
     await element.check()
 
     await checkoutPage.setPayment("braintree")
@@ -614,6 +614,8 @@ test.describe("customer with Braintree", () => {
     await checkoutPage.page.click("[data-test-id=customer-card]", {
       force: true,
     })
+
+    await checkoutPage.page.waitForTimeout(2000)
 
     await checkoutPage.save("Payment", undefined, true)
 
