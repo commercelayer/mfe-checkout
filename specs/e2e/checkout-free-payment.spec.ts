925ef260d61eeb3ec8e75aa8a45e3482bb7b6906
Matteo Alessani <matteo@commercelayer.io>
2022-05-13 14:13:35 +0200

    test: add free sku, single shipping method test

--- specs/e2e/checkout-free-payment.spec.ts
+++ specs/e2e/checkout-free-payment.spec.ts
@@ -1,3 +1,5 @@
+import { usAddress } from "specs/utils/addresses"
+
 import { test, expect } from "../fixtures/tokenizedPage"
 
 test.describe("with shippable sku", () => {
@@ -16,6 +18,51 @@ test.describe("with shippable sku", () => {
     await checkoutPage.setCustomerMail()
 
     await checkoutPage.setBillingAddress()
+    await checkoutPage.checkStep("Customer", "open")
+
+    await checkoutPage.save("Customer")
+
+    await checkoutPage.checkStep("Shipping", "open")
+
+    await checkoutPage.checkShippingSummary("To be calculated")
+
+    expect(checkoutPage.page.locator("text=Standard Shipping")).toBeVisible()
+    await checkoutPage.selectShippingMethod({ text: "Standard Shipping" })
+
+    await checkoutPage.checkShippingSummary("FREE")
+    await checkoutPage.save("Shipping")
+
+    await checkoutPage.checkStep("Payment", "close")
+
+    const element = await checkoutPage.page.locator(
+      "[data-test-id=step-header-info] >> text=This order does not require payment"
+    )
+
+    expect(element).toBeVisible()
+
+    await checkoutPage.save("Payment")
+
+    await checkoutPage.checkPaymentRecap("This order did not require a payment")
+  })
+})
+
+test.describe("with shippable sku single shipping method", () => {
+  test.use({
+    defaultParams: {
+      order: "with-items",
+      market: process.env.NEXT_PUBLIC_MARKET_ID_SINGLE_SHIPPING_METHOD,
+      lineItemsAttributes: [{ sku_code: "TESLAFREE", quantity: 1 }],
+    },
+  })
+
+  test("should execute a free checkout with shipment", async ({
+    checkoutPage,
+  }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.setCustomerMail()
+
+    await checkoutPage.setBillingAddress(usAddress)
     await checkoutPage.checkStep("Customer", "open")
 
     await checkoutPage.save("Customer")
