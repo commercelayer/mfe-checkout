f88bb92c02c5e2cd065983db0ab04ccb0b1843ae
Matteo Alessani <matteo@commercelayer.io>
2022-05-25 18:34:15 +0200

    test: add discount with shipping method selected

--- specs/e2e/shipments.spec.ts
+++ specs/e2e/shipments.spec.ts
@@ -783,3 +783,50 @@ test.describe("discount with coupon", () => {
     await checkoutPage.checkShippingSummary("€10,00")
   })
 })
+
+test.describe("adding coupon code", () => {
+  test.use({
+    defaultParams: {
+      order: "with-items",
+      lineItemsAttributes: [
+        { sku_code: "TSHIRTMMFFFFFFE63E74MXXX", quantity: 1 },
+      ],
+      orderAttributes: {
+        customer_email: customerEmail,
+      },
+      addresses: {
+        billingAddress: euAddress,
+        sameShippingAddress: true,
+      },
+    },
+  })
+
+  test("on delivery step", async ({ checkoutPage }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.checkStep("Customer", "close")
+    await checkoutPage.checkStep("Shipping", "open")
+    await checkoutPage.selectShippingMethod({ text: "Standard Shipping" })
+    await checkoutPage.checkSelectedShippingMethod({ value: true })
+    await checkoutPage.checkButton({ type: "Shipping", status: "enabled" })
+    await checkoutPage.setCoupon("testcoupon")
+    await checkoutPage.checkSelectedShippingMethod({ value: false })
+    await checkoutPage.checkButton({ type: "Shipping", status: "disabled" })
+  })
+
+  test("on payment step", async ({ checkoutPage }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.checkStep("Customer", "close")
+    await checkoutPage.checkStep("Shipping", "open")
+    await checkoutPage.selectShippingMethod({ text: "Standard Shipping" })
+    await checkoutPage.checkSelectedShippingMethod({ value: true })
+    await checkoutPage.checkButton({ type: "Shipping", status: "enabled" })
+    await checkoutPage.save("Shipping")
+    await checkoutPage.checkStep("Payment", "open")
+    await checkoutPage.setCoupon("testcoupon")
+    await checkoutPage.checkStep("Shipping", "open")
+    await checkoutPage.checkSelectedShippingMethod({ value: false })
+    await checkoutPage.checkButton({ type: "Shipping", status: "disabled" })
+  })
+})
