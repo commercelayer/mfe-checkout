121bd058501629092e569a1e46d70289836ef69b
Matteo Alessani <matteo@commercelayer.io>
2022-06-09 12:49:59 +0200

    feat: add German translations

--- components/data/i18n/index.ts
+++ components/data/i18n/index.ts
@@ -1,9 +1,10 @@
 import i18n, { use } from "i18next"
+import translationDE from "public/static/locales/de/common.json"
 import translationEN from "public/static/locales/en/common.json"
 import translationIT from "public/static/locales/it/common.json"
 import { initReactI18next } from "react-i18next"
 
-const languages = ["en", "it"]
+const languages = ["en", "it", "de"]
 
 const resources = {
   en: {
@@ -11,6 +12,9 @@ const resources = {
   },
   it: {
     translation: translationIT,
+  },
+  de: {
+    translation: translationDE,
   },
 }
 
