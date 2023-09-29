93743be0f8a4d049a352d6589631bc19488e21eb
Matteo Alessani <matteo@commercelayer.io>
2023-09-29 14:48:14 +0200

    feat: add native Klarna support

---
+++ specs/e2e/payments-klarna.spec.ts
@@ -0,0 +1,55 @@
+import { faker } from "@faker-js/faker"
+import { euAddress } from "specs/utils/addresses"
+
+import { test, expect } from "../fixtures/tokenizedPage"
+
+const customerEmail = faker.internet.email().toLocaleLowerCase()
+
+test.use({
+  defaultParams: {
+    order: "with-items",
+    lineItemsAttributes: [
+      { sku_code: "BABYONBU000000E63E7412MX", quantity: 2 },
+    ],
+    orderAttributes: {
+      customer_email: customerEmail,
+    },
+  },
+})
+
+test("should execute a checkout with klarna", async ({ checkoutPage }) => {
+  await checkoutPage.checkOrderSummary("Order Summary")
+
+  await checkoutPage.setCustomerMail()
+  await checkoutPage.setBillingAddress({ ...euAddress, phone: "+393293399456" })
+
+  await checkoutPage.checkStep("Customer", "open")
+
+  await checkoutPage.save("Customer")
+
+  await checkoutPage.checkStep("Shipping", "open")
+
+  await checkoutPage.checkShippingSummary("To be calculated")
+
+  await expect(
+    checkoutPage.page.locator("text=Standard Shipping")
+  ).toBeVisible()
+  await checkoutPage.selectShippingMethod({ text: "Standard Shipping" })
+
+  await checkoutPage.checkShippingSummary("FREE")
+  await checkoutPage.save("Shipping")
+
+  await checkoutPage.checkStep("Payment", "open")
+
+  await checkoutPage.selectPayment("klarna")
+
+  await checkoutPage.save("Payment", undefined, true)
+
+  await checkoutPage.completePayment({ type: "klarna" })
+
+  await checkoutPage.checkPaymentRecap("Klarna")
+
+  await checkoutPage.page.reload()
+
+  await checkoutPage.checkPaymentRecap("Klarna")
+})
