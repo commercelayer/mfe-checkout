1973fa899d431b4737e29b8585c018043cb25416
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-08-12 15:19:39 +0200

    feat(REN-398): add partnerSettings mapping

--- utils/getPartnerSettings.ts
+++ utils/getPartnerSettings.ts
@@ -1,20 +1,30 @@
+import { SettingsGlobalAppStoryblok } from "@typings/storyblok";
+
+const DEFAULT_PARTNER_SETTINGS: PartnerSettings =  {brandColors: {dark: "#000A1C", accent: "#003596", base: "#F1F2F9" }}
+
+
 export async function getPartnerSettings(partnerId: string) {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
-    const url = `${baseUrl}/api/partner-settings/${partnerId}`
-    console.log("Fetching partner settings from:", url)
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-    })
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+       try {const response = await fetch(`${baseUrl}/api/partner-settings/${partnerId}`, { method: "GET", headers: { "Content-Type": "application/json" }});
+       const data: {story: {content: SettingsGlobalAppStoryblok}} =await response.json();
+       if (!data?.story?.content) {
+        throw new Error("Settings not found");
+       }
+       return mapPartnerThemeFromSettings(data.story.content);
+     } catch (error) {
+        console.error("Error fetching partner settings:", error);
+        console.info("Returning default settings");
+        return DEFAULT_PARTNER_SETTINGS;
+     }
+   
+}
 
-    // console.log("Fetching partner settings for:", partnerId);
-    // console.log("Response status:", response.status);
-    // console.log("Response headers:", response.headers);
-    // if (!response.ok) {
-    //     throw new Error(`Failed to fetch partner settings: ${response.statusText}`);
-    // }
-    const data = await response.json();
-    console.log("Response data:", data);
-
-    return data;
+export function mapPartnerThemeFromSettings(data: SettingsGlobalAppStoryblok) {
+    return {
+        brandColors: {
+            dark: data?.dark || DEFAULT_PARTNER_SETTINGS.dark,
+            accent: data?.accent || DEFAULT_PARTNER_SETTINGS.accent,
+            base: data?.base || DEFAULT_PARTNER_SETTINGS.base,
+        }
+    }
 }
