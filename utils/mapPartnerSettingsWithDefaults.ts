ed2add188bd118795744777ed3a6615a1f572da3
Alessandro Tecchio <122892593+suppa96@users.noreply.github.com>
2025-08-14 09:59:26 +0200

    fix(REN-398): fix import

--- utils/mapPartnerSettingsWithDefaults.ts
+++ utils/mapPartnerSettingsWithDefaults.ts
@@ -1,6 +1,6 @@
 import { SettingsGlobalAppStoryblok } from "@typings/storyblok";
 
-const DEFAULT_PARTNER_SETTINGS: PartnerSettings =  {brandColors: {dark: "#000A1C", accent: "#003596", base: "#F1F2F9" }, headerLogo: {alt: '', image: ''}};
+export const DEFAULT_PARTNER_SETTINGS: PartnerSettings =  {brandColors: {dark: "#000A1C", accent: "#003596", base: "#F1F2F9" }, headerLogo: {alt: '', image: ''}};
 
 export function mapPartnerSettingsWithDefaults(data: NullableType<SettingsGlobalAppStoryblok>) {
     return {
