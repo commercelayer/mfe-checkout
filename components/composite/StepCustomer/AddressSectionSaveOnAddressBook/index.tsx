6373e7220a68cfb905fe8d479751cb153d4ee5ee
Matteo Alessani <matteo@commercelayer.io>
2025-10-06 20:37:07 +0200

    lint: remove ts-expect error

--- components/composite/StepCustomer/AddressSectionSaveOnAddressBook/index.tsx
+++ components/composite/StepCustomer/AddressSectionSaveOnAddressBook/index.tsx
@@ -1,32 +1,31 @@
-import { AddressInput } from "@commercelayer/react-components"
-import { FlexContainer } from "components/ui/FlexContainer"
-import { CheckCss } from "components/ui/form/CheckBox"
-import { Label } from "components/ui/Label"
-import { useTranslation } from "react-i18next"
+import { AddressInput } from "@commercelayer/react-components";
+import { FlexContainer } from "components/ui/FlexContainer";
+import { CheckCss } from "components/ui/form/CheckBox";
+import { Label } from "components/ui/Label";
+import { useTranslation } from "react-i18next";
 
 interface Props {
-  addressType: "billing" | "shipping"
+  addressType: "billing" | "shipping";
 }
 
 export const AddressSectionSaveOnAddressBook: React.FC<Props> = ({
   addressType,
 }) => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   const fieldName =
     addressType === "billing"
       ? "billing_address_save_to_customer_book"
-      : "shipping_address_save_to_customer_book"
+      : "shipping_address_save_to_customer_book";
   const dataTestId =
     addressType === "billing"
       ? "billing_address_save_to_customer_address_book"
-      : "shipping_address_save_to_customer_address_book"
+      : "shipping_address_save_to_customer_address_book";
 
   return (
     <FlexContainer className="items-center">
       <AddressInput
         data-testid={dataTestId}
-        // @ts-expect-error Missing attribute
         name={fieldName}
         value={"true"}
         id={fieldName}
@@ -40,5 +39,5 @@ export const AddressSectionSaveOnAddressBook: React.FC<Props> = ({
         textLabel={t("stepCustomer.saveAddressBook")}
       />
     </FlexContainer>
-  )
-}
+  );
+};
