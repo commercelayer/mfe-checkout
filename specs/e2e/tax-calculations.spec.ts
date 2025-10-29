5405f82839f454a50e83045827208d0eabc29ea9
Matteo Alessani <matteo@commercelayer.io>
2025-10-29 10:16:04 +0100

    test: add test for tax calculation on shipping address change

--- specs/e2e/tax-calculations.spec.ts
+++ specs/e2e/tax-calculations.spec.ts
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker"
 
 import { expect, test } from "../fixtures/tokenizedPage"
-import { euAddress, euAddressNoBillingInfo } from "../utils/addresses"
+import { euAddressNoBillingInfo } from "../utils/addresses"
 
 const customerEmail = faker.internet.email().toLocaleLowerCase()
 
@@ -50,6 +50,69 @@ test.describe("with different tax calculators", () => {
     await checkoutPage.selectCountry("billing_address", "NO")
 
     await checkoutPage.save("Customer")
+
+    await checkoutPage.checkTaxSummary("2,75 Kr")
+  })
+
+  test("Checkout address changing from Italy to Norway on shipping address", async ({
+    checkoutPage,
+  }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    let email = checkoutPage.getCustomerMail()
+
+    await expect(email).toHaveValue(customerEmail)
+
+    await checkoutPage.changeCustomerEmail("customer@example.com")
+    await checkoutPage.blurCustomerEmail()
+    email = checkoutPage.getCustomerMail()
+
+    await expect(email).toHaveValue("customer@example.com")
+    await checkoutPage.checkStep("Customer", "open")
+
+    await checkoutPage.setBillingAddress(euAddressNoBillingInfo)
+
+    await checkoutPage.save("Customer")
+
+    await checkoutPage.page.waitForTimeout(1000)
+    await checkoutPage.checkStep("Customer", "close")
+
+    await checkoutPage.checkCustomerEmail("customer@example.com")
+
+    await checkoutPage.checkTaxSummary("0,00 Kr")
+
+    await checkoutPage.clickStep("Customer")
+    await checkoutPage.checkStep("Customer", "open")
+
+    await checkoutPage.selectCountry("billing_address", "NO")
+
+    await checkoutPage.save("Customer")
+    await checkoutPage.page.waitForTimeout(1000)
+    await checkoutPage.checkStep("Customer", "close")
+
+    await checkoutPage.checkTaxSummary("2,75 Kr")
+
+    await checkoutPage.clickStep("Customer")
+    await checkoutPage.checkStep("Customer", "open")
+
+    await checkoutPage.shipToDifferentAddress()
+
+    await checkoutPage.setShippingAddress(euAddressNoBillingInfo)
+
+    await checkoutPage.save("Customer")
+    await checkoutPage.page.waitForTimeout(1000)
+    await checkoutPage.checkStep("Customer", "close")
+
+    await checkoutPage.checkTaxSummary("0,00 Kr")
+
+    await checkoutPage.clickStep("Customer")
+    await checkoutPage.checkStep("Customer", "open")
+
+    await checkoutPage.selectCountry("shipping_address", "NO")
+    await checkoutPage.save("Customer")
+    await checkoutPage.page.waitForTimeout(1000)
+
+    await checkoutPage.checkStep("Customer", "close")
 
     await checkoutPage.checkTaxSummary("2,75 Kr")
   })
