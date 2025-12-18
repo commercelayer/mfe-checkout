64b7a3eae760f01e17a09905623dcca1454a07b8
Matteo Alessani <matteo@commercelayer.io>
2025-12-18 18:10:15 +0100

    test: add discount test with checkout.com

--- specs/e2e/payments-checkoutcom.spec.ts
+++ specs/e2e/payments-checkoutcom.spec.ts
@@ -406,7 +406,7 @@ test.describe("guest with checkout.com as single payment method", () => {
     await checkoutPage.checkPaymentRecap("Visa ending in 4242")
   })
 
-  test("should execute a checkout with discount", async ({ checkoutPage }) => {
+  test("should execute a checkout with giftcard", async ({ checkoutPage }) => {
     await checkoutPage.checkOrderSummary("Order Summary")
 
     await checkoutPage.checkTotalAmount("€99,00")
@@ -416,6 +416,42 @@ test.describe("guest with checkout.com as single payment method", () => {
     await checkoutPage.checkGiftCardAmount("-€5,00")
 
     await checkoutPage.checkTotalAmount("€94,00")
+
+    await checkoutPage.setPayment("checkout_com")
+
+    await checkoutPage.save("Payment", undefined, true)
+
+    await checkoutPage.page
+      .frameLocator('iframe[name="cko-3ds2-iframe"]')
+      .locator("#password")
+      .fill("Checkout1!")
+
+    await checkoutPage.page
+      .frameLocator('iframe[name="cko-3ds2-iframe"]')
+      .locator("text=Continue")
+      .click()
+
+    await checkoutPage.page
+      .locator("text=Thank you for your order!")
+      .waitFor({ state: "visible", timeout: 100000 })
+
+    await checkoutPage.checkPaymentRecap("Visa ending in 4242")
+
+    await checkoutPage.page.reload()
+
+    await checkoutPage.checkPaymentRecap("Visa ending in 4242")
+  })
+
+  test("should execute a checkout with coupon discount", async ({ checkoutPage }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.checkTotalAmount("€99,00")
+
+    await checkoutPage.setCoupon("testcoupon")
+
+    await checkoutPage.checkCouponCode("testcoupon")
+
+    await checkoutPage.checkTotalAmount("€89,00")
 
     await checkoutPage.setPayment("checkout_com")
 
