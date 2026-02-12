f712e26343021eef02f6258923bbff19b0b41bc3
Matteo Alessani <matteo@commercelayer.io>
2026-02-12 15:20:17 +0100

    test: add giftcard covering total with adyen partial payments

--- specs/e2e/payments-adyen-givex.spec.ts
+++ specs/e2e/payments-adyen-givex.spec.ts
@@ -104,8 +104,8 @@ test.describe("guest with Adyen using givex", () => {
   })
 
   test("Checkout order with givex, coupon and credit card and expired order", async ({
-      checkoutPage,
-    }) => {
+    checkoutPage,
+  }) => {
     await checkoutPage.checkOrderSummary("Order Summary")
 
     await checkoutPage.checkStep("Payment", "open")
@@ -139,8 +139,6 @@ test.describe("guest with Adyen using givex", () => {
     await checkoutPage.checkPaymentRecap("Visa ending in 1111")
     await checkoutPage.page.reload()
     await checkoutPage.checkPaymentRecap("Visa ending in 1111")
-
-
   })
 })
 
@@ -181,7 +179,9 @@ test.describe("guest with Adyen using givex", () => {
     await checkoutPage.checkPaymentRecap("Giftcard ending in 0000")
   })
 
-  test("Checkout order only with givex with order expired", async ({ checkoutPage }) => {
+  test("Checkout order only with givex with order expired", async ({
+    checkoutPage,
+  }) => {
     await checkoutPage.checkOrderSummary("Order Summary")
 
     await checkoutPage.checkStep("Payment", "open")
@@ -201,5 +201,46 @@ test.describe("guest with Adyen using givex", () => {
     await checkoutPage.checkPaymentRecap("Giftcard ending in 0000")
     await checkoutPage.page.reload()
     await checkoutPage.checkPaymentRecap("Giftcard ending in 0000")
+  })
+})
+
+test.describe("giftcard covering total amount and single adyen gateway with partial payments", () => {
+  test.use({
+    defaultParams: {
+      order: "with-items",
+      lineItemsAttributes: [
+        { sku_code: "CANVASAU000000FFFFFF1824", quantity: 1 },
+      ],
+      giftCardAttributes: {
+        balance_cents: 10000,
+        apply: false,
+      },
+      market: "IT4",
+      addresses: {
+        billingAddress: euAddress,
+        sameShippingAddress: true,
+      },
+    },
+  })
+
+  test("should execute a free checkout", async ({ checkoutPage }) => {
+    await checkoutPage.checkOrderSummary("Order Summary")
+
+    await checkoutPage.setCustomerMail("customer@tk.com")
+    await checkoutPage.checkStep("Customer", "open")
+
+    await checkoutPage.save("Customer")
+    await checkoutPage.checkStep("Shipping", "open")
+
+    await checkoutPage.checkTotalAmount("99,00")
+
+    await checkoutPage.page.waitForTimeout(5000)
+    await checkoutPage.setCoupon(checkoutPage.getGiftCard() as string)
+
+    await checkoutPage.checkGiftCardAmount("-€99,00")
+    await checkoutPage.checkTotalAmount("€0,00")
+
+    await checkoutPage.save("Payment")
+    await checkoutPage.checkPaymentRecap("This order did not require a payment")
   })
 })
