dc3b819b89ac75c8526350599971f3fd145d02f7
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2026-09-25 10:51:05 +0200

    fix: downgrade baseline-browser-mapping to version 2.11.25 and remove minimumReleaseAgeExclude entries

--- components/hooks/useSettingsOrInvalid.ts
+++ components/hooks/useSettingsOrInvalid.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useNavigate, useParams, useSearchParams } from "react-router-dom"
+import { useNavigate, useParams, useSearchParams } from "react-router"
 import { getSettings } from "utils/getSettings"
 import { getSubdomain } from "utils/getSubdomain"
 
